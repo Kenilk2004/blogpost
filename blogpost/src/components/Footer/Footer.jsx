babf6df5fdaf596e029beeba0b96bce634fdddab
Kenilk2004 <167296267+Kenilk2004@users.noreply.github.com>
2025-08-17 23:00:56 +0530

    Update Footer.jsx

--- blogpost/src/components/Footer/Footer.jsx
+++ blogpost/src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
                             </div>
                             <div>
                                 <p className="text-sm text-gray-1000">
-                                    &copy; Copyright 2023. All Rights Reserved by DevUI.
+                                    &copy; Copyright 2024. All Rights Reserved by Kenil.
                                 </p>
                             </div>
                         </div>
